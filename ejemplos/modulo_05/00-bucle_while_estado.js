e0b558f3e08a1d5ef00bb1bd8c9b22dee5ffdec4
Enrique <ebarra@dit.upm.es>
2016-01-30 12:00:20 +0100

    v0

---
+++ ejemplos/modulo_05/00-bucle_while_estado.js
@@ -0,0 +1,8 @@
+var n = 3; // 2^n
+var res =  1;
+while (n !== 0) {
+  res = res * 2;
+  n = n - 1;
+}
+var z = res - 4;
+.....
---
+++ ejemplos/modulo_05/02-bucle_for_estado.js
@@ -0,0 +1,7 @@
+
+var n = 3; // 2^n
+for (var res = 1; n !== 0; n--) {
+  res = res * 2;
+}
+var z = res - 4;
+.....
